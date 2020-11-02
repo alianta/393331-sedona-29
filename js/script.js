59bd8e8cdd7fe93af11acc4e9d3242cabfdb7730
Natalia Khrustaleva <hrustalyova@mail.ru>
2020-11-02 22:54:16 +0300

    добавлена дрожание окна на ошибку в форме

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 const hotelsSearchButton = document.getElementById("hotels-search");
 const hotelsSearchForm = document.getElementById("hotels-search-form");
+const hotelsSearchCheckInDate = document.getElementById("search-form__check-in-date");
+const hotelsSearchCheckOutDate = document.getElementById("search-form__check-out-date");
+
 hotelsSearchForm.classList.add("visually-hidden");
 
+/* Открытие формы */
 hotelsSearchButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   hotelsSearchForm.classList.toggle("visually-hidden");
   hotelsSearchForm.classList.toggle("search-form--show");
+  hotelsSearchForm.classList.remove("search-form--error");
+});
+
+/* Проверка формы на ошибки перед отпавркой */
+hotelsSearchForm.addEventListener("submit", function(evt) {
+  if (hotelsSearchCheckInDate.value == "" || hotelsSearchCheckOutDate.value == "") {
+    evt.preventDefault();
+    hotelsSearchForm.classList.remove("search-form--error");
+    hotelsSearchForm.offsetWidth = hotelsSearchForm.offsetWidth;
+    hotelsSearchForm.classList.add("search-form--error");
+  }
 });
 
 /* Интерактивная карта с индивидуальным маркером в виде логотипа */
